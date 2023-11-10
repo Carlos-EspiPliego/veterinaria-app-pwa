e8648e581e21ae83d402bbd521279975a9474207
Moonchild290404 <2022378001@uteq.edu.mx>
2023-11-10 17:16:23 -0600

    fix:arregla problemas de vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-    Checker({ types: ['dependencies', 'devDependencies'] }),],
+    ],
   resolve: {
     alias: {
       '@components': '/src/components/',
